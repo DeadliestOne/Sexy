2d8b3d6443e3e6b26b817bd5dc0f473d621ccc46
DeadliestOne <uncountableaura@gmail.com>
2024-11-21 02:03:58 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("BeAkatsuki")
+                await self.two.join_chat("ThunderBoltFantasy")
             except:
                 pass
             assistants.append(2)
